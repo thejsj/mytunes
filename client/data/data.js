923e86245371818eb546a331a286e3b4301f1bbe
thejsj <jorge.silva@thejsj.com>
2014-11-12 14:26:06 -0800

    Add timeline for tasks

--- client/data/data.js
+++ client/data/data.js
@@ -16,51 +16,51 @@ var songData = [{
   title: "If Your Girl Only Knew",
   artist: "Aaliyah",
 }, {
-  url: "https://thejsj.com/2014/songs/psy-gangnam_style.mp3",
+  url: "http://thejsj.com/2014/songs/psy-gangnam_style.mp3",
   title: "Gangnam Style",
   artist: "PSY",
 }, {
-  url: "https://thejsj.com/2014/songs/pharrell_williams-happy.mp3",
+  url: "http://thejsj.com/2014/songs/pharrell_williams-happy.mp3",
   title: "Happy",
   artist: "Pharrell Williams",
 }, {
-  url: "https://thejsj.com/2014/songs/taylor_swift-shake_it_off.mp3",
+  url: "http://thejsj.com/2014/songs/taylor_swift-shake_it_off.mp3",
   title: "Shake it Off",
   artist: "Taylor Swift",
 }, {
-  url: "https://thejsj.com/2014/songs/earth_wind_and_fire-september.mp3",
+  url: "http://thejsj.com/2014/songs/earth_wind_and_fire-september.mp3",
   title: "September",
   artist: "Earth, Wind, and Fire",
 }, {
-  url: "https://thejsj.com/2014/songs/lo-dawn_of_midi.mp3",
+  url: "http://thejsj.com/2014/songs/lo-dawn_of_midi.mp3",
   title: "Dawn Of Midi",
   artist: "Lo",
 }, {
-  url: "https://thejsj.com/2014/songs/yuck-souther_skies.mp3",
+  url: "http://thejsj.com/2014/songs/yuck-souther_skies.mp3",
   title: "Southern Skies",
   artist: "Yuck",
 }, {
-  url: "https://thejsj.com/2014/songs/jorge_ben_jorge-taj_mahal.mp3",
+  url: "http://thejsj.com/2014/songs/jorge_ben_jorge-taj_mahal.mp3",
   title: "Taj Mahal",
   artist: "Jorge Ben Jor",
 }, {
-  url: "https://thejsj.com/2014/songs/elis_regina_tom_jobin-aguas_de_marco.mp3",
+  url: "http://thejsj.com/2014/songs/elis_regina_tom_jobin-aguas_de_marco.mp3",
   title: "Aguas de Marco",
   artist: "Ellis Regina & Tom Jobin",
 }, {
-  url: "https://thejsj.com/2014/songs/aventura-solor_por_un_beso.mp3",
+  url: "http://thejsj.com/2014/songs/aventura-solor_por_un_beso.mp3",
   title: "Solo por un beso",
   artist: "Aventura",
 }, {
-  url: "https://thejsj.com/2014/songs/benny_sing-little_donna.mp3",
+  url: "http://thejsj.com/2014/songs/benny_sing-little_donna.mp3",
   title: "Little Donna",
   artist: "Benny Sings",
 }, {
-  url: "https://thejsj.com/2014/songs/allman_brothers_band-ramblin-man.mp3",
+  url: "http://thejsj.com/2014/songs/allman_brothers_band-ramblin-man.mp3",
   title: "Ramblin' Man",
   artist: "Allman Brothers Band",
 }, {
-  url: "https://thejsj.com/2014/songs/casa_azul-superguay.mp3",
+  url: "http://thejsj.com/2014/songs/casa_azul-superguay.mp3",
   title: "Superguay",
   artist: "Casa Azul",
 }];
